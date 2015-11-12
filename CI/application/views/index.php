969fde4f04aad08e28ab7bffcf01f9cd3961a74c
B Esmond <bsey7c@mail.missouri.edu>
2015-11-12 08:15:35 -0600

    PHP
    -Qui-Gon Jinn

--- CI/application/views/index.php
+++ CI/application/views/index.php
@@ -1,7 +1,7 @@
 
 <h1>Log In</h1>
 <!--code igniter handling args/form open and base_url-->
-<?  $this->load->helper('form');
+<php?  $this->load->helper('form');
 	$form_attributes = array('class' => 'login', 'id' => 'login');
 	echo form_open('welcome/index', $form_attributes);
 	$user_data = array('name' => 'username',
@@ -21,13 +21,12 @@
 	echo form_submit($submit_data);
 	echo form_close();
 	echo '<br /><br />';
-	
+
 	$register_attributes = array('class' => 'register', 'id' => 'register');
 	echo form_open('welcome/register', $register_attributes);
-	$register_data = array('name' => 'submit', 
+	$register_data = array('name' => 'submit',
 							'id' => 'submit',
 							'value' => 'Register a New Username Here');
 	echo form_submit($register_data);
 	echo form_close();
 ?>
-	
